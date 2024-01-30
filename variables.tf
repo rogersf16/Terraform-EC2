c71b651eac8555c822d072df5a756a4b9074af5e
rogersf16 <rogersallesfaria16@gmail.com>
2024-01-30 17:50:44 -0300

    Adicionando variables.tf e gitignore

---
+++ variables.tf
@@ -0,0 +1,92 @@
+variable "ami_ubuntu" {
+  type        = string 
+  description = "AMI do O.S ubuntu"
+}
+
+variable ami_redhat {
+  type        = string
+  description = "AMI do O.S Red Hat"
+}
+
+variable tags_vpc {
+    type   = object({
+        Name = string
+    })
+    description = "tags da vpc"
+}
+
+variable tags_instance {
+    type = object({
+        Name = string
+    })
+    description = "tags da instancia"
+}
+
+variable tags_subnet_pub {
+    type = object({
+        Name = string
+    })
+    description = "tags da subnet"
+}
+
+variable tags_subnet_priv {
+    type = object({
+        Name = string
+    })
+    description = "tags da subnet"
+}
+
+variable tags_IGTW {
+    type = object({
+        Name = string
+    })
+    description = "tags do internet gateway"
+}
+
+variable tags_rtb_pub {
+type = object({
+    Name - string
+})
+description - "tags da router table publica"
+}
+
+variable "tags_rtb_priv" {
+    type = object({
+        Name = string
+    })
+description = "tags da router table privada"
+}
+
+variable "tags_sg" {
+    type = object({
+        Name = string
+    })
+description = "Tag do security group"
+}
+
+variable "nome_sg" {
+  type        = string 
+  description = "Nome do Security group criado"
+}
+
+
+
+
+
+
+
+
+/*
+variable SG_BlocoTeste {
+  type        = string
+  description = "Teste de SG por variavel"
+}
+
+variable tags_acl {
+    type = object({
+        Name = string
+    })
+    description = "tags da acl"
+}
+*/
+
